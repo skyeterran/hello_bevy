721003f1b0348b79322136f4a17468b6b0f2399f
skyeterran <skyeterran@gmail.com>
2023-03-31 23:14:51 -0700

    ECS

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+
 fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+           .add_system(hello_world)
+           .add_system(greet_people);
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Skye".to_string())));
+    commands.spawn((Person, Name("Salad".to_string())));
+    commands.spawn((Person, Name("Soup".to_string())));
+}
+
+fn hello_world() {
     println!("Hello, world!");
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("Hi there, {}!", name.0);
+    }
 }

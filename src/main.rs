4d541c1ce59b692c1d3b5e46df81b74826d12657
skyeterran <skyeterran@gmail.com>
2023-04-04 19:14:37 -0700

    Added a movable critter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use bevy::prelude::*;
 
+// Physics timestep
+const TIMESTEP: f32 = 1.0 / 60.0;
+const HAND_SPEED: f32 = 100.0;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
@@ -11,35 +15,121 @@ pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-           .add_startup_system(add_people)
-           .add_system(greet_people);
+        app.add_startup_system(hello_setup)
+           .add_system(move_hand)
+           .add_system(update_tracker)
+           .insert_resource(FixedTime::new_from_secs(TIMESTEP))
+           .insert_resource(Tracker { distance: 0.0 });
     }
 }
 
-#[derive(Component)]
-struct Person;
+fn hello_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(HandBundle::new(&asset_server));
 
-#[derive(Component)]
-struct Name(String);
+    let text_style = TextStyle {
+        font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+        font_size: 40.0,
+        color: Color::WHITE,
+        ..default()
+    };
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Skye".to_string())));
-    commands.spawn((Person, Name("Salad".to_string())));
-    commands.spawn((Person, Name("Soup".to_string())));
+    commands.spawn(
+        TextBundle::from_sections([
+            TextSection::new(
+                "Distance traveled: ",
+                text_style.clone(),
+            ),
+            TextSection::from_style(text_style.clone()),
+        ])
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            position: UiRect {
+                top: Val::Px(10.0),
+                left: Val::Px(10.0),
+                ..default()
+            },
+            ..default()
+        }),
+    );
 }
 
 #[derive(Resource)]
-struct GreetTimer(Timer);
+struct Tracker {
+    distance: f32,
+}
 
-fn greet_people(
-    time: Res<Time>,
-    mut timer: ResMut<GreetTimer>,
-    query: Query<&Name, With<Person>>
-) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in &query {
-            println!("Hi there, {}!", name.0);
+#[derive(Component)]
+struct Collider;
+
+#[derive(Component)]
+struct Hand;
+
+#[derive(Bundle)]
+struct HandBundle {
+    sprite_bundle: SpriteBundle,
+    collider: Collider,
+    hand: Hand,
+}
+
+impl HandBundle {
+    fn new(asset_server: &Res<AssetServer>) -> Self {
+        Self {
+            sprite_bundle: SpriteBundle {
+                texture: asset_server.load("images/Streetlet.png"),
+                transform: Transform {
+                    scale: Vec3::splat(0.25),
+                    ..default()
+                },
+                ..default()
+            },
+            collider: Collider,
+            hand: Hand,
         }
     }
+}
+
+fn move_hand(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut tracker: ResMut<Tracker>,
+    mut query: Query<&mut Transform, With<Hand>>,
+) {
+    let mut hand_transform = query.single_mut();
+    let mut direction: Vec2 = Vec2::new(0.0, 0.0);
+
+    if keyboard_input.pressed(KeyCode::A) {
+        direction.x -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::D) {
+        direction.x += 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::S) {
+        direction.y -= 1.0;
+    }
+
+    if keyboard_input.pressed(KeyCode::W) {
+        direction.y += 1.0;
+    }
+
+    if direction.length_squared() > 1.0 {
+        direction = direction.normalize();
+    }
+
+    let delta = direction * HAND_SPEED * TIMESTEP;
+
+    let new_hand_pos = hand_transform.translation + Vec3::new(delta.x, delta.y, 0.0);
+
+    hand_transform.translation = new_hand_pos;
+
+    tracker.distance += delta.length();
+}
+
+fn update_tracker(
+    tracker: Res<Tracker>,
+    mut query: Query<&mut Text>,
+) {
+    let mut text = query.single_mut();
+    text.sections[1].value = ((tracker.distance / 100.0) as usize).to_string();
 }
